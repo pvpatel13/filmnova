d7d4b735a69f195f45259655e582d033dbda429b
Pushkar Vaghasiya <pushkarvaghasiya@gmail.com>
2026-03-18 09:34:38 +0530

    updated api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'http://localhost:5000/api/';
+const BACKEND_URL = `${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/`;
 
 const omdbApi = {
   get: (url, config) => {
