e40680190ed26918a5b474779d998623ce5b8534
Pushkar Vaghasiya <pushkarvaghasiya@gmail.com>
2026-03-31 09:04:29 +0530

    msg

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 
-var connectDB = function() {
-  var mongoURI = process.env.MONGO_URI;
+var connectDB = function () {
+  var mongoURII = process.env.MONGO_URI;
 
-  mongoose.connect(mongoURI)
-    .then(function(conn) {
+  mongoose.connect(mongoURII)
+    .then(function (conn) {
       console.log('MongoDB Connected: ' + conn.connection.host);
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.error('Error: ' + error.message);
       process.exit(1);
     });
